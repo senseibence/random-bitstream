5d5b881046db724560e47d732fe9b57c034e50f8
Bence Lukácsy <bence.lukacsy@gmail.com>
2022-12-05 01:32:30 -0500

    Update randombitstream.py

--- randombitstream.py
+++ randombitstream.py
@@ -9,7 +9,6 @@ def roundNumber(number):
 
 def generateBitstream():
   
-    # 3 loops just for convenience sake, code should be shortened by using more functions
     for i in range(5,125):
         if (i < 10): im = Image.open(r'C:\Users\bence\Pictures\Saved Pictures\FramebyFrame\ezgif-frame-00'+str(i)+'.jpg')
         elif (i < 100): im = Image.open(r'C:\Users\bence\Pictures\Saved Pictures\FramebyFrame\ezgif-frame-0'+str(i)+'.jpg')
@@ -43,70 +42,9 @@ def generateBitstream():
         
         Bitstream.append(allDigits)
 
-    # the follow loops are the exact same as the above; focus on the above
-    for i in range(1,93):
-        if (i < 10): im = Image.open(r'C:\Users\bence\Pictures\Saved Pictures\FramebyFrame2\ezgif-frame-00'+str(i)+'.jpg')
-        elif (i < 100): im = Image.open(r'C:\Users\bence\Pictures\Saved Pictures\FramebyFrame2\ezgif-frame-0'+str(i)+'.jpg')
-
-        pixels_of_image = list(im.getdata())
-
-        pixelCount = 0
-        sum = 0
-        length = 0
-
-        for pixel in pixels_of_image:
-            pixelCount += 1
-            for colorValue in pixel:
-                sum += colorValue
-                length += 1
-
-        average = sum/length
-        decimalFloat = average - math.floor(average)
-    
-        decimalString = str(decimalFloat)
-        decimalString = decimalString[2:-1]
-    
-        allDigits = [int(digit) for digit in decimalString]
-        
-        for i in range(len(allDigits)):
-            if (allDigits[i] % 2 == 0): allDigits[i] = 0
-            else: allDigits[i] = 1
-        
-        Bitstream.append(allDigits)
-
-    for i in range(1,86):
-        if (i < 10): im = Image.open(r'C:\Users\bence\Pictures\Saved Pictures\FramebyFrame3\ezgif-frame-00'+str(i)+'.jpg')
-        elif (i < 100): im = Image.open(r'C:\Users\bence\Pictures\Saved Pictures\FramebyFrame3\ezgif-frame-0'+str(i)+'.jpg')
-
-        pixels_of_image = list(im.getdata())
-
-        pixelCount = 0
-        sum = 0
-        length = 0
-
-        for pixel in pixels_of_image:
-            pixelCount += 1
-            for colorValue in pixel:
-                sum += colorValue
-                length += 1
-
-        average = sum/length
-        decimalFloat = average - math.floor(average)
-    
-        decimalString = str(decimalFloat)
-        decimalString = decimalString[2:-1]
-    
-        allDigits = [int(digit) for digit in decimalString]
-        
-        for i in range(len(allDigits)):
-            if (allDigits[i] % 2 == 0): allDigits[i] = 0
-            else: allDigits[i] = 1
-        
-        Bitstream.append(allDigits)
-
 generateBitstream()
 Bitstream = [bit for pixel in Bitstream for bit in pixel]
-print(' '.join(str(bit) for bit in Bitstream))
+print(''.join(str(bit) for bit in Bitstream)) # '' --> ' ' if you want a space between bits
 
 zeroCount = 0
 oneCount = 0
@@ -117,17 +55,3 @@ for bit in Bitstream:
 print()
 print('# of 0s: '+str(zeroCount))
 print('# of 1s: '+str(oneCount))
-print()
-
-testingArray = Bitstream
-for i in range(len(Bitstream)):
-    if Bitstream[i] == 0: testingArray[i] = -1
-
-testSum = 0
-for num in testingArray:
-    testSum += num
-
-S_obs = testSum / (math.pow(len(testingArray),(1/2)))
-for_erfc = S_obs / (math.pow(2,1/2))
-
-print(for_erfc)
